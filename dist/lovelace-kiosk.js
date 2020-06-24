40671a6a3f58c150a563b91a36b25786623f1aab
Nicolas Bourasseau <nicolas.bourasseau@outlook.com>
2020-06-24 16:18:27 +0200

    Update lovelace-kiosk.js

--- dist/lovelace-kiosk.js
+++ dist/lovelace-kiosk.js
@@ -1,4 +1,17 @@
-if (window.location.href.indexOf('kiosk') > 0) {
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+          tmp = item.split("=");
+          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
+
+if (findGetParameter("kiosk") != null) {
     setTimeout(function () {
         try {
             const home_assistant_main =  document
